9b205c9eba2cb4b7b6afa6317e238793691cbf67
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:48:47 +0300

    Week8-Functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumbers(20, 6);
+            DivideTwoNumbers(20, 6);
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+
+        private static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("sisestage perekonnanimi:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("sisesta sünniaasta:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+
+            Greetings(userName, userAge, lastName);
+        }
+
+        private static void Greetings(string someString, int someInt, string otherString)
+        {
+            Console.WriteLine($"Tere {someString} {otherString}!");
+            Console.WriteLine($"Oled {2021 - someInt} aastat vana.");
+        }
+
+
+    }
+}
